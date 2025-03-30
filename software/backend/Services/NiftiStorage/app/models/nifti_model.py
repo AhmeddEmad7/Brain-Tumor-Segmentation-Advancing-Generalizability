5da157b2f2aaab5ec88b01f558c4d261b38ca108
Hazem Zakaria <zhazem390@gmail.com>
2025-03-30 08:34:43 +0200

    cover files deleted

---
+++ software/backend/Services/NiftiStorage/app/models/nifti_model.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String, DateTime, Boolean, JSON, TIMESTAMP, func
+from sqlalchemy.ext.declarative import declarative_base
+from app.core.database import Base
+class NiftiModel(Base):
+    __tablename__ = 'nifti_files'
+    
+    id = Column(Integer, primary_key=True, index=True)
+    file_name = Column(String, index=True)
+    file_path = Column(String, index=True)
+    file_size = Column(Integer, index=True)  
+    subject = Column(String, index=True)    # New column for subject
+    session = Column(String, index=True)    # New column for session
+    modality = Column(String, index=True)
+    uploaded_at = Column(TIMESTAMP, server_default=func.now())
+    data_meta  = Column(JSON)
+    is_deleted = Column(Boolean, default=False)

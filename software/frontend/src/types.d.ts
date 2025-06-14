0305bd740f24eb4595872cd708a5c74e52fc1024
Hazem Zakaria <zhazem390@gmail.com>
2025-06-15 00:46:18 +0300

    update tsconfig

--- software/frontend/src/types.d.ts
+++ software/frontend/src/types.d.ts
@@ -1,4 +1,6 @@
 import { PaletteColor } from '@mui/material/styles';
+declare module 'dcmjs';
+declare module '@cornerstonejs/dicom-image-loader';
 
 declare module '@mui/material/styles' {
     interface PaletteColor {
